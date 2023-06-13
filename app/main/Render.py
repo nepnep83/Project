38ab74b6526aa848c409c7ec728324baebde83ae
thomaspowell <thomas.powell@engineering.digital.dwp.gov.uk>
2023-06-13 11:28:54 +0100

    Setup flask

---
+++ app/main/Render.py
@@ -0,0 +1,61 @@
+import os
+import uuid
+
+from flask import Flask, render_template, request, redirect, url_for, session
+import csv
+from flask_wtf.csrf import CSRFError
+from werkzeug.exceptions import HTTPException
+
+from app.main import bp
+from app.main.forms import CookiesForm
+
+app = Flask(__name__)
+user_info = "user_info"
+user_account = "user_account"
+
+
+@bp.route("/")
+def index():
+    return render_template("index.html")
+
+
+@bp.route("/cookies", methods=["GET", "POST"])
+def cookies():
+    form = CookiesForm()
+    # Default cookies policy to reject all categories of cookie
+    cookies_policy = {"functional": "no", "analytics": "no"}
+
+    if form.validate_on_submit():
+        # Update cookies policy consent from form data
+        cookies_policy["functional"] = form.functional.data
+        cookies_policy["analytics"] = form.analytics.data
+
+        # Create flash message confirmation before rendering template
+        flash("You’ve set your cookie preferences.", "success")
+
+        # Create the response so we can set the cookie before returning
+        response = make_response(render_template("cookies.html", form=form))
+
+        # Set cookies policy for one year
+        response.set_cookie("cookies_policy", json.dumps(cookies_policy), max_age=31557600)
+        return response
+    elif request.method == "GET":
+        if request.cookies.get("cookies_policy"):
+            # Set cookie consent radios to current consent
+            cookies_policy = json.loads(request.cookies.get("cookies_policy"))
+            form.functional.data = cookies_policy["functional"]
+            form.analytics.data = cookies_policy["analytics"]
+        else:
+            # If conset not previously set, use default "no" policy
+            form.functional.data = cookies_policy["functional"]
+            form.analytics.data = cookies_policy["analytics"]
+    return render_template("cookies.html", form=form)
+
+
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.debug = True
+    app.run()
