4fd240d8a5757db9285497af5c8043cf4468a219
thomaspowell <thomas.powell@engineering.digital.dwp.gov.uk>
2023-06-22 14:56:26 +0100

    Edited code based on comments

--- app/main/custom_validators.py
+++ app/main/custom_validators.py
@@ -1,22 +1,3 @@
-from wtforms.validators import InputRequired, Length, Regexp
-
-
-class RequiredIf(InputRequired, Regexp, Length):
-    def __init__(self, other_field_name, other_field_value, *args, **kwargs):
-        self.other_field_name = other_field_name
-        self.other_field_value = other_field_value
-
-        super(RequiredIf, self).__init__(*args, **kwargs)
-
-    def __call__(self, form, field):
-        other_field = form._fields.get(self.other_field_name)
-
-        if other_field is None:
-            raise Exception('no field named "%s" in form' % self.other_field_name)
-
-        if other_field.data == self.other_field_value:
-            Length(max=100, message="Input must not be more than 100 characters"),
-            super(RequiredIf, self).__call__(form, field)
 
 
 class ConditionalValidation:
@@ -44,5 +25,3 @@ class ConditionalValidation:
         if other_field.data == self.other_field_value:
             for validation in self.validations:
                 validation.__call__(form, field)
-
-
