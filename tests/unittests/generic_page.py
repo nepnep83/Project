283b39591edd24101505d16d402d85267714e90f
thomaspowell <thomas.powell@engineering.digital.dwp.gov.uk>
2023-06-26 16:14:59 +0100

    TLP-52 spike

--- tests/unittests/generic_page.py
+++ tests/unittests/generic_page.py
@@ -10,8 +10,11 @@ class Elements:
         self.fieldset_heading = context.browser.find_element(By.CLASS_NAME, "govuk-fieldset__heading")
         self.radio_hint = context.browser.find_element(By.ID, "radio-hint")
         self.radio = context.browser.find_element(By.ID, "radio")
+        self.radio_label = context.browser.find_element(By.XPATH,
+                                                        '//*[@id="main-content"]/div/div/form/div/fieldset/div[2]/div[1]/label')
+        self.radio_label2 = context.browser.find_element(By.XPATH,
+                                                        '//*[@id="main-content"]/div/div/form/div/fieldset/div[2]/div[3]/label')
         self.radio_2 = context.browser.find_element(By.ID, "radio-2")
         self.button = context.browser.find_element(By.ID, "submit")
-
 
     pass
