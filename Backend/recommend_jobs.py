1b16f6dcbd9fa1d35d62f890f90e35f2b8cb968d
thomaspowell <thomas.powell@engineering.digital.dwp.gov.uk>
2023-07-12 14:31:35 +0100

    filter the use of common/low value skills

--- Backend/recommend_jobs.py
+++ Backend/recommend_jobs.py
@@ -33,30 +33,27 @@ def soc_to_onet(soc):
     return onet_response['onetCodes'][0]['code']
 
 
-def get_skills_from_onet_code(onet, how_many_skills):
+def get_skills_from_onet_code(onet):
     skills_response = requests.get('https://api.lmiforall.org.uk/api/v1/o-net/skills/' + str(onet), verify=False)
     skills = skills_response.json()['scales'][0]['skills']
     skills.sort(reverse=True, key=common.key)
-    return skills[:how_many_skills]
-
-
-def get_interests_from_onet_code(onet, how_many_interests):
-    interests_response = common.api_call('https://api.lmiforall.org.uk/api/v1/o-net/interests/' + onet)
-    interests = interests_response['scales'][1]['interests']
-    interests.sort(reverse=True, key=common.key)
-    return interests[:how_many_interests]
+    return skills
 
 
 def add_new_skills_and_sort(skills, new_skills):
     skills_id = get_skills_ids(skills)
 
     for new_skill in new_skills:
+        if new_skill['id'] in ['2.A.1.a', '2.A.1.c', '2.A.1.e']:
+            new_skill['value'] *= 0.5
+
         if skills_id.count(new_skill['id']) == 0:
             skills_id.append(new_skill['id'])
             skills.append(new_skill)
         else:
             for skill in skills:
                 if skill['id'] == new_skill['id']:
+
                     skill['value'] += new_skill['value']
 
     skills.sort(reverse=True, key=common.key)
@@ -92,27 +89,38 @@ def get_recommended_jobs(recommended_soc_codes, no_of_jobs):
     return recommended_jobs
 
 
-def run(jobs, how_many_skills):
+def run(jobs, no_of_jobs):
     skills = []
+    top_skills = []
 
     try:
         for job in jobs:
             soc = get_soc_code(job)
             onet = soc_to_onet(soc)
 
-            new_skills = get_skills_from_onet_code(onet, how_many_skills)
+            new_skills = get_skills_from_onet_code(onet)
             add_new_skills_and_sort(skills, new_skills)
     except Exception as e:
         print(e)
 
-    if len(jobs) > 0:
-        top_skills = [skill['id'] for skill in skills[:how_many_skills]]
-        return get_recommended_soc_codes(top_skills)
+    print('here ', skills)
+    if skills:
+        minimum_value = skills[0]['value'] - skills[round(len(skills) * 0.70)]['value']
+        maximum_value = skills[0]['value']  # - skill_list[len(skill_list)-1]['value']
+        print(skills)
+        print(minimum_value)
+        print(maximum_value)
+        for skill in skills:
+            print(skill)
+            print(skill['value'])
+            if minimum_value <= skill['value'] <= maximum_value:
+                print('output')
+                top_skills.append(skill['id'])
+        rev_experience = get_recommended_soc_codes(top_skills)
+        return get_recommended_jobs(rev_experience, no_of_jobs)
 
 
 if __name__ == "__main__":
-    _range = 15
-    interests_range = 5
     job_num = 5
 
-    run(['Plumber'], _range)
+    run(['Plumber', 'engineer', 'lawyer'], job_num)
